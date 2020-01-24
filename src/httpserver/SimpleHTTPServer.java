fa0cc238507243116f85a6dc9df135899d820e9a
Dan Brennan <DanBrennan33@hotmail.com>
2020-01-23 21:05:27 -0500

    Update with output to acknowledge when connection is accepted.

--- src/httpserver/SimpleHTTPServer.java
+++ src/httpserver/SimpleHTTPServer.java
@@ -1,18 +1,25 @@
 package httpserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SimpleHTTPServer {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		final ServerSocket server = new ServerSocket(8080);
 		System.out.println("Listening for connection on port 8080 ....");
 		while (true) {
-			final Socket client = server.accept();
-			// 1. Read HTTP request from the client socket
-			// 2. Prepare an HTTP response
-			// 3. Send HTTP response to the client
-			// 4. Close the socket
+			Socket clientSocket = server.accept();
+			InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+			BufferedReader reader = new BufferedReader(isr);
+			String line = reader.readLine();
+			while (!line.isEmpty()) {
+				System.out.println(line);
+				line = reader.readLine();
+			}
+            
 		}
 	}
 }
